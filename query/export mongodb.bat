2841ffc0766bc67349a466f0c3fd028cf985d5b5
Cuong <cuongvip1295@gmail.com>
2018-03-12 00:19:36 +0700

    query mongo backup

---
+++ query/export mongodb.bat
@@ -0,0 +1,5 @@
+C:
+
+cd C:\Program Files\MongoDB\Server\3.4\bin
+
+mongoexport -d HotelBookingReservationsSystem -c my-hotel-reservation -o D:\thesis-in-process\my-hotel\query\my-hotel-reservation.json
